9f418ed3d467a0373d903d94ca501b0bd28c7f7c
bryan@thebrocode.io <Boots@Bryans-MacBook-Air.local>
2015-09-11 11:15:41 -1000

    first attempt

---
+++ stack_trace.js
@@ -0,0 +1,13 @@
+function addText() {
+  var inputText = document.getElementById('textBox').value;
+  return ll.insert(inputText, 0);
+}
+
+function showText() {
+  var show = '';
+  ll.reset();
+  while(ll.hasNext()) {
+    str +=
+  }
+  document.getElementById('history').textContent = document.getElementById('history').innerHTML;
+}
